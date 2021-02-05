718121fbdc8a271de370c52e7dc98e8c157ff44a
MaximeMalecot <92maximemalecot@gmail.com>
2021-02-05 12:36:25 +0100

    Text Component added

---
+++ src/Components/Text.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function Text(){
+    return(
+        <p>Aute est sint nostrud in ex ea Lorem mollit dolore aliqua officia amet. Eu amet excepteur deserunt commodo amet aliqua anim excepteur aliquip culpa. Ullamco qui irure exercitation elit laboris pariatur esse aliquip eiusmod magna cupidatat officia laborum et. Non dolore do amet dolor aute. Culpa voluptate amet aute id nostrud. Laboris ut ad aliquip eiusmod est nulla ex adipisicing esse qui officia cupidatat laborum.</p>
+    )
+}
